b3c5b39f476e3e8d48cc52ae40103ba8e66dd2d9
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2024-09-24 22:59:56 +0300

    First program using Python.

---
+++ Demo1.py
@@ -0,0 +1,19 @@
+print ('Welcome to Python!')
+name = input('type your name: ')
+print ('Hello, ' + name + '!' + ' Welcome to the program!')
+color = input('What is your favorite color? ')
+print ("Your favorite color is " + color)
+print ("Wow, you're awesome!")
+age = input('What is your Birth year? ')
+print("You're age the age of:"), print (2024 - int(age))
+weight = input ("What is your weight in pound?")
+print("Your weight in kilogram is: ")
+print(0.45 * float(weight))
+
+print('''
+This is the first program made in python by Abram.
+      Looking forward to understand more about this,
+      and looking forward to make more.
+
+      This is just the beginning of my Joruney. Thank You!
+''')
