ffd1dcb86b0c483cfccaf1ecff48a2b781d982a2
ApramPepo <120373555+ApramPepo@users.noreply.github.com>
2024-10-01 01:31:02 +0300

    cleaning code

--- Demo1.py
+++ Demo1.py
@@ -2,14 +2,13 @@
 
 print ('Welcome to Python!')
 name = input('type your name: ')
-name = name.strip()
-name = name.title()
+name = name.strip().title()
 print ('Hello, ' + name + '!' + ' Welcome to the program!')
 
 # Ask users about their information
 
 color = input('What is your favorite color? ')
-color = color.capitalize(), color = color.strip()
+color = color.strip().capitalize()
 print ("Your favorite color is " + color, end="? ")
 print ("Wow, you're awesome!")
 age = input('What is your Birth year? ')
@@ -21,9 +20,12 @@ print(0.45 * float(weight))
 # Add location
 
 country = input("Which country do you live in? ")
-country = country.strip()
-country = country.capitalize()
-print ("Cool to know you're from ", country, end="!")
+country = country.strip().capitalize()
+print ("Cool to know you're from ", country, end=" !")
+
+city = input("Would you mind if you tell us which city? ")
+city = city.strip().capitalize()
+print (city,"?", "Wow! Awesome!")
 
 print('''
 This is the first program made in python by Abram.
